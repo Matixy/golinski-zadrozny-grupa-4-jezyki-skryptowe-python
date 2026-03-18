18f08304553655393bf59d188a6a750e5202ea46
Mateusz Zadrożny <mateusz.zadrozny2004@wp.pl>
2026-03-18 01:27:28 +0100

    Stworzenie funkcji getQuestionsAndExclamationsSenteces
    Funkcja, która wypisuje na wyjściu tylko zdania, które są pytaniami lub wykrzyknieniami

---
+++ Lista2/src/filterFunctions/getQuestionsAndExclamationsSenteces.py
@@ -0,0 +1,23 @@
+import sys
+from utils.textTools import generateSentences
+
+def getQuestionsAndExclamationsSenteces(stream) -> str:
+  """Funkcja, która wypisuje na wyjściu tylko zdania, które są pytaniami lub wykrzyknieniami"""
+  
+  res: str = ""
+  
+  for sentence in generateSentences(stream):
+    if sentence[len(sentence)-1] == "!" or sentence[len(sentence)-1] == "?":
+      res += sentence + "\n"
+  
+  return res
+
+def main():
+  #ustawienie kodowania na utf-8
+  sys.stdin.reconfigure(encoding='utf-8')
+  
+  result: str = getQuestionsAndExclamationsSenteces(sys.stdin)
+  print(result)
+
+if __name__ == '__main__':
+  main()
