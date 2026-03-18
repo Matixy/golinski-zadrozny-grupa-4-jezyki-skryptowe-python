4357d063f2463891a504cae48f08ee20df0637a3
Mateusz Zadrożny <mateusz.zadrozny2004@wp.pl>
2026-03-18 12:32:42 +0100

    Stworzenie errorHandler.py
    dodanie Funkcji ktora uruchamia i obsluguje potencjalne wyjatki metody podanej w parametrze

---
+++ Lista2/src/utils/errorHandler.py
@@ -0,0 +1,26 @@
+import sys
+import os
+
+VALUE_ERROR_INFO: str = "Błąd danych:"
+DEFAULT_ERROR_INFO: str = "Błąd programu:"
+
+def runFuncWithExceptionHandling(func):
+  """Funkcja ktora uruchamie obsluguje potencjalne wyjatki metody podanej w parametrze """
+  
+  try:
+    func()
+  except ValueError as e:
+    print(f"{VALUE_ERROR_INFO} {e}", file=sys.stderr)
+    sys.exit(1)
+  except BrokenPipeError:
+    # wyjście przy pękniętym potoku- zignorowanie błędu
+    try:
+        devnull = os.open(os.devnull, os.O_WRONLY)
+        os.dup2(devnull, sys.stdout.fileno())
+    except Exception:
+        pass
+    sys.exit(0)
+    
+  except Exception as e:
+    print(f"{DEFAULT_ERROR_INFO} {e}", file=sys.stderr)
+    sys.exit(1)
