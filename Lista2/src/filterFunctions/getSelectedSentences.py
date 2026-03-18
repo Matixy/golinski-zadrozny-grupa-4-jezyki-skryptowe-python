35258a124d825ecd17478eacd7f8d77f7938453d
Mateusz Zadrożny <mateusz.zadrozny2004@wp.pl>
2026-03-18 11:53:47 +0100

    Stworzenie getSelectedSentences
    dodanie Funkcji wypisująca tylko zdania, które zawierają co najmniej dwa wyrazy z następujących: „i”, „oraz”, „ale”, „że”, „lub”

--- Lista2/src/filterFunctions/getSelectedSentences.py
+++ Lista2/src/filterFunctions/getSelectedSentences.py
@@ -1,7 +1,9 @@
 import sys
 from src.utils.textTools import generateSentences, getWord
 
-def getSelectedSentences(stream) -> str:
+APPEARANCES_SELECTED_SIGNS: int = 2
+
+def getSelectedSentences(stream, appearances: int) -> str:
   """
   Funkcja wypisująca tylko zdania, które zawierają co najmniej dwa wyrazy z
   następujących: „i”, „oraz”, „ale”, „że”, „lub”
@@ -11,16 +13,29 @@ def getSelectedSentences(stream) -> str:
   
   for sentence in generateSentences(stream):
     counterSelectedWords: int = 0
+    tempSentence: str = sentence
+    word: str = getWord(tempSentence)
     
-    while sentence != "":
-      word: str = getWord(sentence)
-      print(word)
+    while word != "":
+      wordToCheck: str = word.lower() # standaryzacja liter gdyby wyraz byly na poczatku zdania
       
-      if word == "i" or word == "oraz" or word == "ale" or word == "że" or word == "lub":
-        print(word)
+      if (
+        wordToCheck == "i" 
+        or wordToCheck == "oraz" 
+        or wordToCheck == "ale" 
+        or wordToCheck == "że" 
+        or wordToCheck == "lub"):
+        counterSelectedWords += 1
         
-      sentence = sentence[:sentence.find(word)]
+      wordStartIndexInSentence: int = tempSentence.find(word)
+      wordEndIndexInSentence: int = wordStartIndexInSentence + len(word)
+      
+      tempSentence = tempSentence[wordEndIndexInSentence:] # uciecie zdania o sprawdzony wyraz
+      word: str = getWord(tempSentence) # przejscie do kolejnego wyrazu
   
+    if counterSelectedWords >= appearances:
+      res += sentence + "\n"
+      
   return res
 
 def main():
@@ -28,7 +43,7 @@ def main():
   sys.stdin.reconfigure(encoding='utf-8')
   sys.stdout.reconfigure(encoding='utf-8')
   
-  result: str = getSelectedSentences(sys.stdin)
+  result: str = getSelectedSentences(sys.stdin, APPEARANCES_SELECTED_SIGNS)
   print(result)
 
 if __name__ == '__main__':
