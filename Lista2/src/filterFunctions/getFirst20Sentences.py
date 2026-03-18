3ad92b65945947a86884f3951934960b2ab31c8f
Mateusz Zadrożny <mateusz.zadrozny2004@wp.pl>
2026-03-18 01:40:56 +0100

    Stworzenie funkcji getFirst20Sentences
    Funkcja, która wypisuje na wyjściu tylko zdania, które są pytaniami lub wykrzyknieniami

---
+++ Lista2/src/filterFunctions/getFirst20Sentences.py
@@ -0,0 +1,29 @@
+import sys
+from utils.textTools import generateSentences
+
+def getFirst20Sentences(stream) -> str:
+  """Funkcja wypisująca pierwszych 20 zdań tekstu"""
+  
+  res: str = ""
+  generator = generateSentences(stream)
+  counter: int = 0
+  
+  while counter != 20:
+    try:
+      sentence = next(generator)
+      res += sentence + "\n"
+      counter += 1
+    except StopIteration: # teskt ma mniej niz 20 zdan zakoncz petle
+        break;
+  
+  return res
+
+def main():
+  #ustawienie kodowania na utf-8
+  sys.stdin.reconfigure(encoding='utf-8')
+  
+  result: str = getFirst20Sentences(sys.stdin)
+  print(result)
+
+if __name__ == '__main__':
+  main()
