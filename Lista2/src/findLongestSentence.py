1ef0fbdca16f7c4258d8bfd0ce7d11d6c224e3d1
Papit00_ <golinski-pawel@wp.pl>
2026-03-18 01:25:16 +0100

    Stworzono findLongestSentence
    Funkcja wypisująca najdłuższe zdanie w książce (kryterium – liczba znaków).

---
+++ Lista2/src/findLongestSentence.py
@@ -0,0 +1,32 @@
+import sys
+from utils.textTools import generateSentences
+
+def findLongestSentence(stream):
+    """Funkcja wypisująca najdłuższe zdanie w książce (kryterium – liczba znaków)."""
+    longestSentence = ""
+    maxSentenceLength = 0
+
+    for sentence in generateSentences(stream):
+        currentSentenceLength = len(sentence)
+        if currentSentenceLength > maxSentenceLength:
+            longestSentence = sentence
+            maxSentenceLength = currentSentenceLength
+
+    if not longestSentence:
+        raise ValueError("Nie znaleziono zadnych zdan w podanym tekscie")
+
+    return longestSentence
+
+def main():
+    try:
+        sys.stdin.reconfigure(encoding='utf-8')
+        resultSentence = findLongestSentence(sys.stdin)
+        print(resultSentence)
+
+    except ValueError as e:
+        print(e, file=sys.stderr)
+    except Exception as e:
+        print(e, file=sys.stderr)
+
+if __name__ == "__main__":
+    main()
