fbdaaec1abcf6f5e28a8007cbb0de3ec7d1ddab5
Mateusz Zadrożny <mateusz.zadrozny2004@wp.pl>
2026-03-23 00:20:46 +0100

    get_top_uris created
    get_top_uris init

---
+++ Lista3/src/get_top_uris.py
@@ -0,0 +1,18 @@
+import read_log
+
+TOP_URI_TO_RETURN: int = 15
+
+def get_top_uris(log: list, n: int = 10) -> list:
+  """returns the n most frequently occurring URIs"""
+  print(log)
+  
+
+def main():
+  print("chuj")
+  # data: list = read_log.read_log(10)
+  # top_uris: list = get_top_uris(data, TOP_URI_TO_RETURN)
+  
+  print("chuj")
+
+if __name__ == "__main__":
+  main()
