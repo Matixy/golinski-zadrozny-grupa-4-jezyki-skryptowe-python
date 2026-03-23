c536cfbd63a0c3952f6719f6da861e0f8b3d3d30
Mateusz Zadrożny <mateusz.zadrozny2004@wp.pl>
2026-03-23 19:18:52 +0100

    init get_most_active_session

---
+++ Lista3/src/get_most_active_session.py
@@ -0,0 +1,15 @@
+import read_log
+from print_dict_entry_dates import get_dict_entry_dates
+
+def get_most_active_session(sessions_log: dict) -> str:
+  """returns the most active session uid"""
+  
+  
+
+def main():
+  data: list = read_log.read_log()    
+  
+  
+
+if __name__ == "__main__":
+  main()
