e0db5f82dcec8a6c55332fcea79e95ba25728ce7
Mateusz Zadrożny <mateusz.zadrozny2004@wp.pl>
2026-03-23 01:04:17 +0100

    get_top_uris done
    function works

--- Lista3/src/get_top_uris.py
+++ Lista3/src/get_top_uris.py
@@ -1,18 +1,30 @@
 import read_log
+import sort_log
 
-TOP_URI_TO_RETURN: int = 15
+TOP_URI_TO_RETURN: int = 10
+URI_INDEX: int = 8
 
 def get_top_uris(log: list, n: int = 10) -> list:
   """returns the n most frequently occurring URIs"""
-  print(log)
+  uri_occurences: dict = {}
+  for row in log:
+    uri = row[URI_INDEX]
+    uri_occurences[uri] = uri_occurences.get(uri, 0) + 1
   
+  uris_occurences_sorted: list = sort_log.sort_log(list(uri_occurences.items()), 1, True)
+  n_uris: list = []
+  
+  for item in uris_occurences_sorted[:n]:
+    n_uris.append(item[0])
+  
+  return n_uris
 
 def main():
-  print("chuj")
-  # data: list = read_log.read_log(10)
-  # top_uris: list = get_top_uris(data, TOP_URI_TO_RETURN)
+  data: list = read_log.read_log()    
+  top_uris: list = get_top_uris(data, TOP_URI_TO_RETURN)
   
-  print("chuj")
+  for i in top_uris:
+    print(i)
 
 if __name__ == "__main__":
   main()
