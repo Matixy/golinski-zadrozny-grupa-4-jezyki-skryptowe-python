81d682918d8066dff893d3d6576b78fd60347bc9
Mateusz Zadrożny <mateusz.zadrozny2004@wp.pl>
2026-04-19 03:29:38 +0200

    init get_addresses

---
+++ Lista5/src/get_addresses.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+
+def get_addresses(path: Path, city: str) -> list[tuple[str, str, str, str]]:
+  return
+
+def main():
+  print()
+
+if __name__ == "__main__":
+    main()
