ef4d11f69fce68d31d9243f17ce166b8b0b4ea68
Papit00_ <golinski-pawel@wp.pl>
2026-04-21 13:37:48 +0200

    Update exceptions.py

--- Lista5/src/utils/exceptions.py
+++ Lista5/src/utils/exceptions.py
@@ -1,15 +1,11 @@
 class CliAppError(Exception):
     pass
 
-class InvalidDateError(CliAppError):
-    pass
-
 class DataNotFoundError(CliAppError):
     pass
 
 class StationNotExist(CliAppError):
     pass
 
-class CalculationError(CliAppError):
-    pass
+
 
